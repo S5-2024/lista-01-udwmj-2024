26ba0ff77904800e8674f386e970fc27acc3b4f2
Gabi <gabrielle.sote@gmail.com>
2024-08-25 21:22:45 -0300

    algumas adições

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Site do PHP</title>
+</head>
+<body>
+
+  <?php 
+ echo "<p>Data e Hora Atual: " . date('d/m/Y H:i:s') . "</p>";
+  ?>
+  
+</body>
+</html>
